0e85729afa34cbe6783143667fb2f251f062359d
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-06-30 09:18:00 -0500

    Importing youtube search and allowing to create objects from search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyCX_ZSBeeU47pYxzXpEkAS1e56Htx9UlQ0'
+const API_KEY = 'AIzaSyCX_ZSBeeU47pYxzXpEkAS1e56Htx9UlQ0';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+})
 
 // Create a new component. This component should produce
 // some HTML
