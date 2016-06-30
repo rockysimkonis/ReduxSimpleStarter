3256fd2dd30908c5d82a8e9657ff111350ba5a73
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-06-30 09:57:33 -0500

    Video list receiving videos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCX_ZSBeeU47pYxzXpEkAS1e56Htx9UlQ0';
 
@@ -23,12 +23,13 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
 }
 
 // Take this component's generated HTML and put it
-// on the page (in the DOM)guillermo jacquez
+// on the page (in the DOM)
 
 ReactDOM.render(<App />, document.querySelector('.container'));
