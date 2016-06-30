196d0e64562483fcbddb1a9e10509cdc5f1bc9ae
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-06-29 19:19:45 -0500

    Added youtube api key and installed dependancy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyCX_ZSBeeU47pYxzXpEkAS1e56Htx9UlQ0'
+
 // Create a new component. This component should produce
 // some HTML
-const App = function() {
+const App = () => {
   return <div>Hi!</div>;
 }
 
