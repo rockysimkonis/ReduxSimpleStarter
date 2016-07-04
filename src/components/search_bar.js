24b27357f7e4a69ee099280c97cbfd4ae8e27b81
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-07-04 18:12:25 -0500

    Adding search term functionality

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-
     this.state = {term: ''};
   }
 
@@ -12,9 +11,14 @@ class SearchBar extends Component {
       <div className='search-bar'>
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
